26fb24fce294cfe9bfd85e1dae35a4ab934822a5
wuthithewin <wuthithewin@gmail.comgit config --global user.email wuthithewin@gmail.comgit config --global user.name>
2019-07-23 18:08:10 -0700

    review on how to create an array and display on html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
-var a = "hello";
-console.log(a);
+var questions = ['food ', 'eat ', 'drinks']
+var p = $('<p>').text("hhhhh")
+$('body').append(questions);
 
+// for (var i=0; i<questionsListItem; i++){
+// questionsListItem = $('<li>');
+// questionsListItem.text(questions[i]);
+// list.append(questionsListItem);
+// }
+// set up a question and answer using form
+// click and check if the answer is correct 
+// create a time function 
+// show the score results
+// return to the game again
+
+// $('p')  //select or target
+// $('<p>') // creating
