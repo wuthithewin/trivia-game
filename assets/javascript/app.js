a4d9be0dff04b4bf52cf8ac1f951986a0ab9ac94
wuthithewin <wuthithewin@gmail.comgit config --global user.email wuthithewin@gmail.comgit config --global user.name>
2019-07-30 19:04:58 -0700

    create a function name renderQuestions and make 2 for loops

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,62 @@
-var questions = ['food ', 'eat ', 'drinks']
-var p = $('<p>').text("hhhhh")
-$('body').append(questions);
+var questions = [
+    {
+        question: 'what is my breakfast',
+        choices: ["brownie", "coffee", "croistant", "bread"],
+        answer: 1
+    },
+    {
+        question: 'How many distance from san jose to sf in miles',
+        choices: ["30 ", "45 ", "50 ", "60 "],
+        answer: 2
+    }
+]
+
+
+// console.log(questions[0].question)
+// console.log(questions[1].question)
+function renderQuestions() {
+    for (var i = 0; i < questions.length; i = i + 1) {
+        console.log(questions[i].question)
+
+        var q = $('<p>')
+        q.text(questions[i].question)
+        $('form').append(q)
+
+        // console.log(questions[i].choices[0])
+        // console.log(questions[i].choices[1])
+        for (var j = 0; j < questions[i].choices.length; j = j + 1) {
+            console.log(questions[i].choices[j])
+            var radio = $('<input>')
+            radio.attr("type", "radio")
+            radio.attr("name", "question" + i)
+            radio.attr("id", "question" + i + "choice" + j)
+            radio.addClass("form-check-input") // radio.attr("class", "form-check-input")
+            radio.val(j)
+            var label = $('<label>')
+            label.attr("for", "question" + i + "choice" + j)
+            label.text(questions[i].choices[j])
+            label.attr("class", "form-check-label")
+            $('form').append(radio, label)
+            
+
+
+        }
+
+        //            <div class="form" 
+        //< input class="form-check-input" type = "radio" name = "exampleRadios" id = "exampleRadios1" value = "option1" checked >
+        //<label class="form-check-label" for="exampleRadios1">
+
+        //   </label>
+        //   </div>
+    }
+}
+renderQuestions()
+
+
 
 // for (var i=0; i<questionsListItem; i++){
 // questionsListItem = $('<li>');
 // questionsListItem.text(questions[i]);
 // list.append(questionsListItem);
-// }
-// set up a question and answer using form
-// click and check if the answer is correct 
-// create a time function 
-// show the score results
-// return to the game again
-
 // $('p')  //select or target
 // $('<p>') // creating
