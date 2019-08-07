29eebc326fa2af2f148be7d50ff1e1e974d7fd87
wuthithewin <wuthithewin@gmail.comgit config --global user.email wuthithewin@gmail.comgit config --global user.name>
2019-08-06 17:58:54 -0700

    add div class, form check

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,25 @@ var questions = [
     }
 ]
 
+var time = 50;
+$('#timing').text(time)
+function countTime() {
+    time--;
+    // num = num-1; / num -= 1;
+    $('#timing').text(time)
+}
+var timer = setInterval(countTime, 1 * 1000);
+// function alertTime() {
+//     if (timer == 0)
+//     { 
+
+//     }
+// }
+// $('#stop').on('click', function () {
+//     clearInterval(timer);
+// });
+// $('#start').on('click', function () {
+//     timer = setInterval(countTime, 1 * 1000);
 
 // console.log(questions[0].question)
 // console.log(questions[1].question)
@@ -18,40 +37,48 @@ function renderQuestions() {
     for (var i = 0; i < questions.length; i = i + 1) {
         console.log(questions[i].question)
 
-        var q = $('<p>')
-        q.text(questions[i].question)
-        $('form').append(q)
+        var pTag = $('<p>') // <p></p>
+        pTag.text(i + 1 + ". " + questions[i].question) // <p>1. what is my breakfast</p>
+        $('form').append(pTag)
 
         // console.log(questions[i].choices[0])
         // console.log(questions[i].choices[1])
         for (var j = 0; j < questions[i].choices.length; j = j + 1) {
             console.log(questions[i].choices[j])
-            var radio = $('<input>')
-            radio.attr("type", "radio")
-            radio.attr("name", "question" + i)
-            radio.attr("id", "question" + i + "choice" + j)
-            radio.addClass("form-check-input") // radio.attr("class", "form-check-input")
+            var radio = $('<input>')  // <input>
+            radio.attr("type", "radio") // <input type="radio">
+            radio.attr("name", "question" + i) // <input type="radio" name="question0">
+            radio.attr("id", "question" + i + "choice" + j) // <input type="radio" name="question0" id="question0choice0">
+            radio.addClass("form-check-input")
+            // radio.attr("class", "form-check-input")
+            // <input type="radio" name="question0" id="question0choice0" class="form-check-input">
             radio.val(j)
-            var label = $('<label>')
-            label.attr("for", "question" + i + "choice" + j)
-            label.text(questions[i].choices[j])
-            label.attr("class", "form-check-label")
-            $('form').append(radio, label)
-            
+            // radio.attr("value", j)
+            // <input type="radio" name="question0" id="question0choice0" class="form-check-input" value="0">
 
+            var label = $('<label>') // <label>
+            label.attr("for", "question" + i + "choice" + j) //  <label for="question0choice0"
+            label.text(questions[i].choices[j])
+            label.attr("class", "form-check-label") // <label for="question0choice0"
+            var divTag = $('<div>')
+            divTag.addClass("form-check")
+            divTag.append(radio, label)
+            $('form').append(divTag)
+            // var divGroup = $('<div>')
+            // divGroup.addClass("form-group", "radio", "label", "divTag")
+            // divGroup.append("")
+            // $('form').append(divGroup)
 
         }
 
-        //            <div class="form" 
-        //< input class="form-check-input" type = "radio" name = "exampleRadios" id = "exampleRadios1" value = "option1" checked >
-        //<label class="form-check-label" for="exampleRadios1">
-
+        //<div class="form-check" >
+        //   < input class="form-check-input" type = "radio" name = "exampleRadios" id = "exampleRadios1" value = "option1" checked >
+        //   <label class="form-check-label" for="exampleRadios1">
         //   </label>
-        //   </div>
+        //</div>
     }
 }
 renderQuestions()
-
 
 
 // for (var i=0; i<questionsListItem; i++){
@@ -59,4 +86,4 @@ renderQuestions()
 // questionsListItem.text(questions[i]);
 // list.append(questionsListItem);
 // $('p')  //select or target
-// $('<p>') // creating
+// $('<p>') // creating 
